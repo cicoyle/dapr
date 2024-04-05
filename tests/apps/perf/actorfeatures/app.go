ab27d18443f49a6349e03c0e0463073a90046196
Artur Souza <asouza.pro@gmail.com>
2024-04-04 22:46:18 -0700

    Add more features to perf-actorfeatures.
    Signed-off-by: Artur Souza <asouza.pro@gmail.com>

--- tests/apps/perf/actorfeatures/app.go
+++ tests/apps/perf/actorfeatures/app.go
@@ -14,18 +14,24 @@ limitations under the License.
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"sync/atomic"
 
+	"github.com/dapr/dapr/tests/e2e/utils"
 	chi "github.com/go-chi/chi/v5"
 )
 
 const (
 	appPort                 = 3000
+	daprHTTPPort            = 3500
 	daprV1URL               = "http://localhost:3500/v1.0"
 	actorMethodURLFormat    = daprV1URL + "/actors/%s/%s/%s/%s"
 	actorSaveStateURLFormat = daprV1URL + "/actors/%s/%s/state/"
@@ -57,6 +63,35 @@ var daprConfigResponse = daprConfig{
 	drainRebalancedActors,
 }
 
+// request for timer or reminder.
+type timerReminderRequest struct {
+	OldName   string `json:"oldName,omitempty"`
+	ActorType string `json:"actorType,omitempty"`
+	ActorID   string `json:"actorID,omitempty"`
+	NewName   string `json:"newName,omitempty"`
+	Data      string `json:"data,omitempty"`
+	DueTime   string `json:"dueTime,omitempty"`
+	Period    string `json:"period,omitempty"`
+	TTL       string `json:"ttl,omitempty"`
+	Callback  string `json:"callback,omitempty"`
+}
+
+// response object from an actor invocation request
+type daprActorResponse struct {
+	Data     []byte            `json:"data"`
+	Metadata map[string]string `json:"metadata"`
+}
+
+// requestResponse represents a request or response for the APIs in this app.
+type response struct {
+	ActorType string `json:"actorType,omitempty"`
+	ActorID   string `json:"actorId,omitempty"`
+	Method    string `json:"method,omitempty"`
+	StartTime int    `json:"start_time,omitempty"`
+	EndTime   int    `json:"end_time,omitempty"`
+	Message   string `json:"message,omitempty"`
+}
+
 func getActorType() []string {
 	actorType := os.Getenv(actorTypeEnvName)
 	if actorType == "" {
@@ -70,6 +105,9 @@ func getActorType() []string {
 	}
 	return actorTypes
 }
+
+var httpClient = utils.NewHTTPClient(true)
+var counter = atomic.Int32{}
 
 // indexHandler is the handler for root path
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -88,6 +126,7 @@ func actorMethodHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Processing actor method request for %s", r.URL.RequestURI())
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	counter.Add(1)
 }
 
 func deactivateActorHandler(w http.ResponseWriter, r *http.Request) {
@@ -104,9 +143,119 @@ func deactivateActorHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// calls Dapr's Actor method/timer/reminder: simulating actor client call.
+func testCallActorHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Processing %s test request for %s", r.Method, r.URL.RequestURI())
+
+	actorType := chi.URLParam(r, "actorType")
+	id := chi.URLParam(r, "id")
+	callType := chi.URLParam(r, "callType")
+	method := chi.URLParam(r, "method")
+
+	url := fmt.Sprintf(actorMethodURLFormat, actorType, id, callType, method)
+
+	log.Printf("Invoking: %s %s\n", r.Method, url)
+	expectedHTTPCode := 200
+	var req timerReminderRequest
+	switch callType {
+	case "method":
+		// NO OP
+	case "timers":
+		fallthrough
+	case "reminders":
+		if r.Method == http.MethodGet {
+			expectedHTTPCode = 200
+		} else {
+			expectedHTTPCode = 204
+		}
+		body, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			log.Printf("Could not get reminder request: %s", err.Error())
+			return
+		}
+
+		log.Println("Body data: " + string(body))
+		json.Unmarshal(body, &req)
+	}
+
+	body, err := httpCall(r.Method, url, req, expectedHTTPCode)
+	if err != nil {
+		log.Printf("Could not read actor's test response: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if len(body) == 0 {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	var response daprActorResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Printf("Could not parse actor's test response: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(response.Data)
+}
+
+func counterHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Processing %s test request for %s", r.Method, r.URL.RequestURI())
+	if r.Method == "DELETE" {
+		counter.Store(0)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(strconv.Itoa(int(counter.Load()))))
+}
+
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(""))
+}
+
+func httpCall(method string, url string, requestBody interface{}, expectedHTTPStatusCode int) ([]byte, error) {
+	var body []byte
+	var err error
+
+	if requestBody != nil {
+		body, err = json.Marshal(requestBody)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != expectedHTTPStatusCode {
+		var errBody []byte
+		errBody, err = io.ReadAll(res.Body)
+		if err == nil {
+			return nil, fmt.Errorf("Expected http status %d, received %d, payload ='%s'", expectedHTTPStatusCode, res.StatusCode, string(errBody)) //nolint:stylecheck
+		}
+
+		return nil, fmt.Errorf("Expected http status %d, received %d", expectedHTTPStatusCode, res.StatusCode) //nolint:stylecheck
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resBody, nil
 }
 
 // appRouter initializes restful api router
@@ -116,6 +265,10 @@ func appRouter() http.Handler {
 	router.Get("/", indexHandler)
 	router.Get("/dapr/config", configHandler)
 
+	// The POST method is used to register reminder
+	// The DELETE method is used to unregister reminder
+	router.HandleFunc("/call/{actorType}/{id}/{callType}/{method}", testCallActorHandler)
+
 	router.Put("/actors/{actorType}/{id}/method/{method}", actorMethodHandler)
 	router.Put("/actors/{actorType}/{id}/method/{reminderOrTimer}/{method}", actorMethodHandler)
 
@@ -123,6 +276,9 @@ func appRouter() http.Handler {
 	router.Delete("/actors/{actorType}/{id}", deactivateActorHandler)
 
 	router.Get("/healthz", healthzHandler)
+
+	router.Get("/counter", counterHandler)
+	router.Delete("/counter", counterHandler)
 
 	router.HandleFunc("/test", fortioTestHandler)
 
--- tests/apps/perf/actorfeatures/go.mod
+++ tests/apps/perf/actorfeatures/go.mod
@@ -2,4 +2,15 @@ module github.com/dapr/dapr/tests/apps/perf/tester
 
 go 1.21
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/dapr/dapr v1.13.2
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	github.com/dapr/kit v0.13.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
