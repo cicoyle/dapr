a58e22dc78e717fc734758fbf6f66b89cbb10ca6
Cassandra Coyle <cassie@diagrid.io>
2024-05-22 11:41:18 -0500

    continuously retry scheduler clients if it fails upon the first try
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -292,10 +292,12 @@ func newDaprRuntime(ctx context.Context,
 	)
 
 	if runtimeConfig.SchedulerEnabled() {
-		rt.schedulerClients, err = clients.New(ctx, clients.Options{
+		opts := clients.Options{
 			Addresses: runtimeConfig.schedulerAddress,
 			Security:  sec,
-		})
+		}
+
+		rt.schedulerClients, err = continuouslyRetrySchedulerClient(ctx, opts)
 		if err != nil {
 			return nil, err
 		}
@@ -381,6 +383,23 @@ func (a *DaprRuntime) Run(parentCtx context.Context) error {
 	}
 
 	return a.runnerCloser.Run(ctx)
+}
+
+func continuouslyRetrySchedulerClient(ctx context.Context, opts clients.Options) (*clients.Clients, error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			cli, err := clients.New(ctx, opts)
+			if err == nil {
+				return cli, nil
+			}
+
+			log.Infof("Failed to initialize scheduler clients: %s. Retrying...", err)
+			time.Sleep(time.Second)
+		}
+	}
 }
 
 func getPodName() string {
