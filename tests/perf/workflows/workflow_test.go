3dfb06bcd25a40dea2e674b134155001d5e964b2
Cassandra Coyle <cassie@diagrid.io>
2024-06-04 12:13:27 -0700

    mv config
    Signed-off-by: Cassandra Coyle <cassie@diagrid.io>

--- tests/perf/workflows/workflow_test.go
+++ tests/perf/workflows/workflow_test.go
@@ -63,8 +63,8 @@ func TestMain(m *testing.M) {
 			AppMemoryLimit:    "800Mi",
 			AppMemoryRequest:  "800Mi",
 			AppPort:           -1,
+			Config:            "featureactorreminderscheduler",
 		},
-		Config: "featureactorreminderscheduler",
 	}
 
 	comps := []kube.ComponentDescription{}
